37dc1571758f4366af7f021d020d961053633a94
ArtyomKr <tyomik.krasnitsky@gmail.com>
2021-10-04 11:43:15 +0300

    feat: solves new tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,5 +14,46 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default function transform(arr) {
+    if (Object.prototype.toString.call(arr) !== '[object Array]') throw new Error("'arr' parameter must be an instance of the Array!");
 
+    let transformed = [].concat(arr);
+
+    transformed.forEach((item, i) => {
+        switch(item) {
+            case '--discard-next':
+                transformed.splice(i, 2);
+                break;
+            case '--discard-prev':
+                if (i === 0) {
+                    transformed.splice(i, 1);
+                    break;
+                } else {
+                   transformed.splice(i-1, 2)
+                break; 
+                }
+            case '--double-next':
+                if (i === transformed.length-1) {
+                    transformed.splice(i, 1);
+                    break;
+                } else {
+                    transformed.splice(i, 1, transformed[i+1])
+                    break;  
+                }
+                
+            case '--double-prev':
+                if (i === 0) {
+                    transformed.splice(i, 1);
+                    break;
+                } else {
+                   transformed.splice(i, 1, transformed[i-1])
+                break; 
+                }
+            default: 
+               break;
+        }
+    })
+
+    return transformed;
 }
+
+console.log(transform([ 1, 2, 3, '--double-prev', 4, 5 ]));
